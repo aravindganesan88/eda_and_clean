57905377797d8ab61352fc1b266ecf5614f52c06
Aravind1988 <ag4077@columbia.edu>
2022-12-04 01:09:39 +0530

    print dataframe for creating test fixtures

--- eda_and_clean/utils.py
+++ eda_and_clean/utils.py
@@ -33,3 +33,18 @@ def generate_outlier_mask_based_on_z_score(df, col_name, z_score_threshold=3):
 
 def filter_non_capitalized_words_from_list(_list: list) -> list:
     return [word for word in _list if word.isupper()]
+
+
+def print_dataframe_as_dataframe_definition(df: pd.DataFrame) -> None:
+    """
+    Prints a dataframe as a dataframe definition.
+    :param df: Pandas dataframe
+    :param df_name: Name of the dataframe
+    :return: None
+    """
+    df_text = f"pd.DataFrame({df.to_dict()})"
+    # replace "TimeStamp" with "pd.to_datetime"
+    df_text = df_text.replace("Timestamp", "pd.to_datetime")
+    # replace "nan" with "np.nan"
+    df_text = df_text.replace("nan", "np.nan")
+    return df_text
